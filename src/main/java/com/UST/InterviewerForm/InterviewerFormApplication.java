85f7d50ec6ae24143ee524dcde32c1979577d943
nimisha173 <nimishars32@gmail.com>
2023-06-24 23:27:31 +0530

    class project spring

---
+++ src/main/java/com/UST/InterviewerForm/InterviewerFormApplication.java
@@ -0,0 +1,13 @@
+package com.UST.InterviewerForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewerFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewerFormApplication.class, args);
+	}
+
+}
